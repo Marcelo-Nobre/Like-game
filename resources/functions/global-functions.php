aa5fed1330b29a0f40778f88ca3865f5587e0ce3
Tiago França <tiago@tiagofranca.com>
2024-03-02 01:06:35 -0300

    improved site-config - added package arandu/laravel-site-options
    On branch task/site-config
    	modified:   composer.json
    	modified:   composer.lock
    	new file:   config/site-options.php
    	new file:   database/migrations/2024_03_02_011505_create_site_options_table.php
    	modified:   resources/functions/global-functions.php

--- resources/functions/global-functions.php
+++ resources/functions/global-functions.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Fluent;
 use Illuminate\Support\Facades\Auth;
 
-if (! function_exists('fluent')) {
+if (!function_exists('fluent')) {
     /**
      * function fluent
      */
@@ -60,5 +60,125 @@ if (!function_exists('userDate')) {
         }
 
         return $carbonTime->format($format);
+    }
+}
+
+if (!function_exists('safe')) {
+    /**
+     * safe function  -- A easy way to try/catch run
+     *
+     * ```php
+     * // Usage:
+     * safe(fn () => 'may be error action', fn (\Throwable $th) => 'catch action here')
+     * ```
+     *
+     * @param Closure $runner
+     * @param Closure|null $throwableCatcher
+     *
+     * @return mixed
+     */
+    function safe(Closure $runner, ?Closure $throwableCatcher = null): mixed
+    {
+        try {
+            return $runner();
+        } catch (Throwable $th) {
+            if (!$throwableCatcher) {
+                return false;
+            }
+
+            return $throwableCatcher($th);
+        }
+    }
+}
+
+if (!function_exists('safeOr')) {
+    /**
+     * safeOr function
+     *
+     * ```php
+     * // Usage:
+     * safeOr(fn () => 'may be error action', 'fail')
+     * safeOr(fn () => 'may be error action', null)
+     * safeOr(fn () => 'may be error action')
+     * safeOr(fn () => 'may be error action', [])
+     * ```
+     *
+     * @param Closure $runner
+     * @param mixed $valueOnFail
+     *
+     * @return mixed
+     */
+    function safeOr(Closure $runner, mixed $valueOnFail = null): mixed
+    {
+        try {
+            return $runner();
+        } catch (Throwable $th) {
+            return $valueOnFail;
+        }
+    }
+}
+
+if (!function_exists('is_unserializable')) {
+    /**
+     * function is_unserializable
+     *
+     * @param mixed $data
+     *
+     * @return bool
+     */
+    function is_unserializable(mixed $data): bool
+    {
+        $data = is_string($data) ? trim($data) : null;
+
+        if (!$data || !(strlen($data) === 2 && $data === 'N;') || strlen($data) < 4) {
+            return false;
+        }
+
+        foreach (['N;', ';', ':'] as $partial) {
+            if (
+                (
+                    str_contains($data[1] ?? '', $partial)
+                    || str_contains($data, $partial)
+                ) && (
+                    str_ends_with($data, ';')
+                    || str_ends_with($data, '}')
+                )
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('try_unserialize')) {
+    /**
+     * function try_unserialize
+     *
+     * @param mixed $data
+     * @param mixed $defaultOnFail
+     * @param null|\Closure $catcher
+     *
+     * @return mixed
+     */
+    function try_unserialize(
+        mixed $data,
+        mixed $defaultOnFail = null,
+        ?Closure $catcher = null
+    ): mixed {
+        try {
+            if (!is_unserializable($data)) {
+                return $defaultOnFail;
+            }
+
+            return unserialize($data);
+        } catch (Throwable $th) {
+            if ($catcher) {
+                $catcher($th);
+            }
+
+            return $defaultOnFail;
+        }
     }
 }
